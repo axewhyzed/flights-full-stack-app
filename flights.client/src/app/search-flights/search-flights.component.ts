bcee3dfc79a4a3be612ae557a698be9717624d04
axewhyzed <coding0234@gmail.com>
2024-01-31 16:31:17 +0530

    Finished working on frontend

--- flights.client/src/app/search-flights/search-flights.component.ts
+++ flights.client/src/app/search-flights/search-flights.component.ts
@@ -6,9 +6,60 @@ import { Component } from '@angular/core';
   styleUrl: './search-flights.component.css'
 })
 export class SearchFlightsComponent {
-  searchResult: any = [
-    "American Airlines",
-    "British Airways",
-    "Lufthansa"
+  searchResult: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: {
+        time: Date.now().toString(),
+        place: "Los Angeles"
+      },
+      arrival: {
+        time: Date.now().toString(),
+        place: "Istanbul"
+      },
+      price: "350"
+    },
+    {
+      airline: "Deutsche BA",
+      remainingNumberOfSeats: 60,
+      departure: {
+        time: Date.now().toString(),
+        place: "Munchen"
+      },
+      arrival: {
+        time: Date.now().toString(),
+        place: "Schiphol"
+      },
+      price: "600"
+    },
+    {
+      airline: "British Airways",
+      remainingNumberOfSeats: 80,
+      departure: {
+        time: Date.now().toString(),
+        place: "London, England"
+      },
+      arrival: {
+        time: Date.now().toString(),
+        place: "Vizzola-Ticino "
+      },
+      price: "500"
+    },
+
+    
   ]
+}
+
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
